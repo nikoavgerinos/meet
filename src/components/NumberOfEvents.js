388e422cb23eeff1e4cb6fde9827957bba36596e
nikoavgerinos <avgerinos.niko@gmail.com>
2024-01-23 17:14:00 +0100

    feat: Applied CSS
    Description:
    - Updated the stylesheet to improve the visual design of the application.
    - Fixed alignment issues with navigation bar elements.
    - Adjusted font sizes for better readability.
    - Added responsive styles for mobile devices.
    - Refactored CSS code for better maintainability.

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
 
-
 const NumberOfEvents = ({ setNumberOfEvents }) => {
     const [eventNumber, setEventNumber] = useState('32');
+
     const handleInputChange = (event) => {
         const value = event.target.value;
         setEventNumber(value);
         setNumberOfEvents(value);
     }
+
     return (
         <div id="numberOfEvents">
+            <label htmlFor="eventNumberInput">Show Events: </label>
             <input
                 type="text"
+                id="eventNumberInput"
                 value={eventNumber}
                 onChange={handleInputChange}
+                style={{
+                    width: "40px", /* Set the width to make it smaller */
+                    textAlign: "center", /* Center the text horizontally */
+
+                }}
             />
         </div>
-    )
+    );
 }
 
 export default NumberOfEvents;
